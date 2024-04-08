7c7b6343acdd574fddd80db62080ef2602937157
AiratNazmiev <nazmiev.airat@yandex.ru>
2024-04-08 15:14:57 +0300

    main added

--- bookkeeper/__main__.py
+++ bookkeeper/__main__.py
@@ -1,3 +1,25 @@
-# запуск poetry run python bookkeeper
+"""
+Запуск проекта:
+poetry run python bookkeeper
+"""
 
-print(42)
+import sys
+from PySide6.QtWidgets import QApplication  # pylint: disable=no-name-in-module
+
+from bookkeeper.presenter.presenter import Presenter as App  # alias for Presenter
+from bookkeeper.view.view import View
+from bookkeeper.repository.repository_factory import repository_factory
+
+DB_FILE = 'db/bookkeeper.db'
+
+if __name__ == "__main__":
+    qapp = QApplication(sys.argv)
+    view = View()
+
+    bookkeeper_app = App(
+        view=view,
+        repository_factory=repository_factory(DB_FILE)
+    )
+    bookkeeper_app.show_window()
+
+    sys.exit(qapp.exec())
