84d327641b778ba54d45628852df6fabda9e783c
AiratNazmiev <nazmiev.airat@yandex.ru>
2024-04-08 20:50:21 +0300

    test_presenter tbd

---
+++ tests/test_presenter/test_presenter.py
@@ -0,0 +1,6 @@
+from pytestqt.qt_compat import qt_api
+import pytest
+
+from bookkeeper.presenter.presenter import Presenter
+from bookkeeper.view.view import View
+from bookkeeper.repository.repository_factory import repository_factory
